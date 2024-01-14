0c53bd565d5868fbb768bd5ebcb879ad82a34109
Francisco Oberti <80848653+franoberti@users.noreply.github.com>
2024-01-14 19:17:07 -0300

    Add NavBar Dinamic

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,97 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styles from './navbar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
 
+  const [visibleNavbar, setVisibleNavBar] = useState(false)
+  
+  const miEtiquetaRef = useRef(null);
+  const miEtiquetaRef2 = useRef(null);
+
+  const handleClickBars = () => {
+    setVisibleNavBar(!visibleNavbar)
+  }
+
+  useEffect(() => {
+    // Función de manejo de clic para el documento
+    function handleDocumentClick(event) {
+      // Verificar si el clic ocurrió fuera de la etiqueta específica
+      if ((visibleNavbar && miEtiquetaRef.current && !miEtiquetaRef.current.contains(event.target)) &&
+        (miEtiquetaRef2.current && !miEtiquetaRef2.current.contains(event.target))) {
+        // Realiza la acción que deseas aquí
+        setVisibleNavBar(false)
+        // Puedes realizar la acción que desees aquí
+      }
+    }
+
+    // Agregar el event listener al documento si visibleNavbar es true
+    if (visibleNavbar) {
+      document.addEventListener('click', handleDocumentClick);
+    }
+
+
+    // Limpiar el event listener al desmontar el componente
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, [visibleNavbar]);
+
   return (
-    <div className={`${styles.navBar}`}>
-      <div className={`container-fluid`}>
+    <>
 
-        <div className={`${styles.navContainer}`}>
+      <div className={`${styles.navBar}`}>
 
-          <a className={`${styles.logo} text-primary`}>Red De Consultoría Interdisciplinaria</a>
-          <a className={`${styles.logoMobile} text-primary`}>RDCI</a>
 
-          <div className={`${styles.items} `}>
-            <div className={`${styles.itemNav}`}>
-              <a className={`${styles.itemNavText}`} href="#razonDeSer">Razon de Ser</a>
+        <div className={`container-fluid`}>
+
+          <div className={`${styles.navContainer}`}>
+
+            <a className={`${styles.logo} text-primary`}>Red De Consultoría Interdisciplinaria</a>
+            <a className={`${styles.logoMobile} text-primary`}>RDCI</a>
+
+            <div className={`${styles.items} `}>
+              <div className={`${styles.itemNav}`}>
+                <a className={`${styles.itemNavText}`} href="#razonDeSer">Razon de Ser</a>
+              </div>
+              <div className={`${styles.itemNav}`}>
+                <a className={`${styles.itemNavText}`} href="#servicios">Servicios</a>
+              </div>
+              <div className={`${styles.itemNav}`}>
+                <a className={`${styles.itemNavText}`} href="#nosotros">Nosotros</a>
+              </div>
             </div>
-            <div className={`${styles.itemNav}`}>
-              <a className={`${styles.itemNavText}`} href="#servicios">Servicios</a>
-            </div>
-            <div className={`${styles.itemNav}`}>
-              <a className={`${styles.itemNavText}`} href="#nosotros">Nosotros</a>
-            </div>
-          </div>
 
-          <div className={`${styles.itemsMobile}`}>
-            <FontAwesomeIcon icon={faBars} className={`${styles.bars}`} />
+            <div className={`${styles.itemsMobile}`}>
+              <FontAwesomeIcon icon={faBars} className={`${styles.bars}`} onClick={handleClickBars}  ref={miEtiquetaRef2} />
+            </div>
+
           </div>
 
         </div>
 
+        {
+          visibleNavbar ?
+
+            <div className={`${styles.optionsMobile}`} ref={miEtiquetaRef}>
+              <div className={`${styles.itemNav}`}>
+                <a className={`${styles.itemNavText}`} href="#razonDeSer">Razon de Ser</a>
+              </div>
+              <div className={`${styles.itemNav}`}>
+                <a className={`${styles.itemNavText}`} href="#servicios">Servicios</a>
+              </div>
+              <div className={`${styles.itemNav}`}>
+                <a className={`${styles.itemNavText}`} href="#nosotros">Nosotros</a>
+              </div>
+            </div>
+            :
+            <></>
+        }
+
       </div>
 
-    </div>
+    </>
 
   )
 }
